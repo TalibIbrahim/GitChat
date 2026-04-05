59a9e64023e2756df81fbc5b04fdab32b850a04c
TalibIbrahim <112410392+TalibIbrahim@users.noreply.github.com>
2026-04-05 14:41:01 +0500

    Implemented ingest route which fetches github repo url, chunks the files, create embeddings and sends it to mongodb

---
+++ app/api/ingest/route.ts
@@ -0,0 +1,48 @@
+import clientPromise from "@/lib/mongodb";
+import { NextResponse } from "next/server";
+import { MongoDBAtlasVectorSearch } from "@langchain/mongodb";
+
+// Fetching and Chunking Logic
+import { fetchGithubRepo } from "@/lib/github/fetcher";
+import { chunkFiles } from "@/lib/langchain/splitter";
+import { getEmbeddings } from "@/lib/langchain/models";
+
+export async function POST(req: Request) {
+  try {
+    const body = await req.json();
+    const { repoUrl } = body; // extract the repo URL from the request body by destructuring
+
+    const files = await fetchGithubRepo(repoUrl); // Fetch the github repo
+    const chunks = await chunkFiles(files); // Chunk the files
+
+    const client = await clientPromise; // Connect to MongoDB Atlas
+    const db = client.db("github_rag");
+    const collection = db.collection("code_chunks");
+
+    // Automatic expiration of documents after 24 hours (86400 seconds)
+    await collection.createIndex(
+      { "metadata.createdAt": 1 },
+      { expireAfterSeconds: 86400 },
+    );
+
+    console.log(`Embedding ${chunks.length} chunks and saving to MongoDB...`);
+
+    // .fromDocuments automatically batches the chunks, sends them to Ollama using our getEmbeddings function to generate the vectors, and saves everything to MongoDB in one step
+    await MongoDBAtlasVectorSearch.fromDocuments(chunks, getEmbeddings(), {
+      collection: collection as any, // Type assertion to bypass type issues with the MongoDB collection
+      indexName: "vector_index",
+      textKey: "text",
+      embeddingKey: "embedding",
+    });
+
+    console.log("Ingestion complete");
+
+    return NextResponse.json({
+      success: true,
+      message: `Successfully embedded and saved ${chunks.length} chunks to the database!`,
+    });
+  } catch (error: any) {
+    console.error("Ingestion Error:", error);
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
---
+++ lib/langchain/splitter.ts
@@ -0,0 +1,26 @@
+import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
+import { GithubFile } from "../github/fetcher";
+
+//   Takes an array of raw GitHub files and breaks them into smaller, AI-digestible chunks while keeping track of where they came from.
+
+export async function chunkFiles(files: GithubFile[]) {
+  // Configuring the Splitter
+
+  const splitter = new RecursiveCharacterTextSplitter({
+    chunkSize: 1000, // Max tokens per chunk (1000 is a safe size for 4GB VRAM)
+    chunkOverlap: 200, // Overlap between chunks to maintain context
+  });
+
+  const rawDocuments = files.map((file) => ({
+    pageContent: file.content,
+    metadata: { source: file.path },
+    createdAt: new Date(),
+  }));
+
+  // Execute the Split
+  console.log(`Splitting ${files.length} files...`);
+  const chunks = await splitter.splitDocuments(rawDocuments); // Breaks down raw files into smaller chunks with metadata about their source and creates an array of chunk objects
+
+  console.log(`Created ${chunks.length} chunks!`);
+  return chunks;
+}
